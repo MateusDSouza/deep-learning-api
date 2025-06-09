f4e565dbf59c5e3d880e74ad6c917689b0ee7f88
Mateus D Souza <desouza.mateus1996@gmail.com>
2025-06-09 13:22:10 +0200

    Separation of concerns - part 2

--- api/tasks/tasks.py
+++ api/tasks/tasks.py
@@ -1,34 +1,53 @@
-from api.models.models import TranslationModel
+from api.models.models import TranslationModel, TextField
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+from api.schemas.schemas import TranslationRequest
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=512)
 translator = T5ForConditionalGeneration.from_pretrained("t5-small")
 
 
-def store_translation(t):
-    model = TranslationModel(
-        text=t.text, base_language=t.base_language, final_language=t.final_language
-    )
-    model.save()
+## IMPORTANT METHOD
+def store_translation(translation_request: TranslationRequest) -> int:
+    model = TranslationModel(**translation_request.model_dump()).save()
     return model.id  # type: ignore
 
 
-def run_translation(t_id: int):
-    model = TranslationModel.get_by_id(t_id)
+def create_payload(model: TranslationModel):
+    return f"translate {model.base_language} to {model.final_language}: {model.text}"
 
-    prefix = f"translate {model.base_language} to {model.final_language}: {model.text}"
+
+## IMPORTANT METHOD
+def run_translation(translation_record_id: int) -> None:
+    # get translation using the id
+    translation_model = get_translation_by_id(translation_record_id)
+    # create payload
+    prefix = create_payload(translation_model)
+    # transform into numbers
     input_ids = tokenizer(prefix, return_tensors="pt").input_ids
-
+    # execute translation
     outputs = translator.generate(input_ids, max_new_tokens=512)
+    # from numbers back to text
     translation = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # using the retrieval object save the translation in the object
+    translation_model.translation = translation
+    # store the object in the database
+    translation_model.save()
 
-    model.translation = translation
-    model.save()
 
+## IMPORTANT METHOD
+def find_translation(translation_record_id: int):
+    translation = get_translation_by_id(translation_record_id).translation
 
-def find_translation(t_id: int):
-    model = TranslationModel.get_by_id(t_id)
-    translation = model.translation
     if translation is None:
         translation = "Processing..."
     return translation
+
+
+def get_translation_by_id(translation_record_id: int) -> TranslationModel:
+    return TranslationModel.get_by_id(translation_record_id).translation
+
+
+# refactor
+def verify_translation(translation: TextField):
+    pass
--- api/v1/endpoints/main.py
+++ api/v1/endpoints/main.py
@@ -7,13 +7,13 @@ app = FastAPI()
 
 @app.get("/")
 def get_root():
-    return {"message": "Hello World!"}
+    return {"message": "Entry of the Translation API!"}
 
 
 @app.post("/translate")
 def post_translation(t: TranslationRequest, background_tasks: BackgroundTasks):
     t_id = tasks.store_translation(t)
-    background_tasks.add_task(tasks.run_translation(t_id))  # type: ignore
+    background_tasks.add_task(tasks.run_translation(t_id))  # type: ignore #
     return {"task_id": t_id}
 
 
